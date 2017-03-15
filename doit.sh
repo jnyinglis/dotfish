a1c74140b43ba5d786f1adc1b7cbadecd4ab4e6a
jnyinglis <john.inglis@me.com>
2017-03-15 09:37:56 -0400

    Corrected shell script and added new alias

--- doit.sh
+++ doit.sh
@@ -1,5 +1,5 @@
 for fish_file in ./functions/*.fish
 do
 	fish_file=${fish_file#*./functions/}
-	[[ ! -L ~/.config/fish/functions/${fish_file} ]] && ln -s $(pwd)/functions/git-create.fish ~/.config/fish/functions/${fish_file}
+	[[ ! -L ~/.config/fish/functions/${fish_file} ]] && ln -s $(pwd)/functions/${fish_file} ~/.config/fish/functions/${fish_file}
 done
---
+++ functions/mit-scheme.fish
@@ -0,0 +1,3 @@
+function mit-scheme --description 'Command line for Scheme'
+	"/Applications/MIT-Scheme.app/Contents/Resources/mit-scheme"
+end
