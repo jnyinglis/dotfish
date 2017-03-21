5e2c9e92191e3fcfad0b7f5bfe1ffd7b62c54c6f
jnyinglis <john.inglis@me.com>
2017-03-21 08:21:31 -0400

    Added fish config file plus updated installer to be aware of file

---
+++ config.fish
@@ -0,0 +1,3 @@
+# Command Line editing
+# Enable vi mode
+fish_vi_key_bindings
--- doit.sh
+++ doit.sh
@@ -4,3 +4,4 @@ do
 	[[ ! -L ~/.config/fish/functions/${fish_file} ]] && ln -s $(pwd)/functions/${fish_file} ~/.config/fish/functions/${fish_file}
 	curl -L https://get.oh-my.fish | fish
 done
+ln -s $(pwd)/config.fish ~/.config/fish/config.fish
