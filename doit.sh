f4e276af360e59dacfa21e782bf8e9907d4809f1
jnyinglis <john.inglis@me.com>
2017-05-03 21:14:14 -0600

    comment out omf install

--- doit.sh
+++ doit.sh
@@ -1,7 +1,8 @@
+#!/usr/local/bin/bash
 for fish_file in ./functions/*.fish
 do
 	fish_file=${fish_file#*./functions/}
 	[[ ! -L ~/.config/fish/functions/${fish_file} ]] && ln -s $(pwd)/functions/${fish_file} ~/.config/fish/functions/${fish_file}
-	curl -L https://get.oh-my.fish | fish
+	#curl -L https://get.oh-my.fish | fish
 done
 ln -s $(pwd)/config.fish ~/.config/fish/config.fish
