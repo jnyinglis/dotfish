b0f4ec3f10191042f5fc3a6dfd101f17a0f5e6c4
jnyinglis <john.inglis@me.com>
2017-05-03 21:09:14 -0600

    add Julia command line repl

--- config.fish
+++ config.fish
@@ -1,3 +1,8 @@
+#
+set -x VIMINIT ":source ~/.vim/.vimrc"
+
+set PATH $PATH $HOME/.cargo/bin
+
 # Command Line editing
 # Enable vi mode
 fish_vi_key_bindings
---
+++ functions/julia.fish
@@ -0,0 +1,3 @@
+function julia --description 'Command line for Julia'
+	"/Applications/Juliai-0.5.app/Contents/Resources/julia/bin/julia"
+end
