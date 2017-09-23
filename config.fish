87d075fe9eaa2e43b66da80b9446b8263b242bf0
jnyinglis <john.inglis@me.com>
2017-09-23 10:18:47 -0400

    Change the content of the initial README.md in git-create

--- config.fish
+++ config.fish
@@ -1,7 +1,9 @@
 #
 set -x VIMINIT ":source ~/.vim/.vimrc"
 
-set PATH $PATH $HOME/.cargo/bin
+set PATH $PATH $HOME/.cargo/bin /opt/local/bin /opt/local/sbin
+
+set MANPATH $MANPATH /opt/local/share/man
 
 # Command Line editing
 # Enable vi mode
--- functions/git-create.fish
+++ functions/git-create.fish
@@ -3,10 +3,10 @@ function git-create --description 'Create a git repo'
 		set repo "$argv[1]"
 		set repodescription "$argv[2]"
 		curl -u 'jnyinglis' https://api.github.com/user/repos -d '{"name":"'"$repo"'","description":"'"$repodescription"'"}'
-		echo "# {$repo}" >> README.md
+		echo "# $repo" >> README.md
 		git init
 		git add README.md
-		git commit -m "First Commit"
+		git commit -m "Create skeleton repo"
 		git remote add origin https://github.com/jnyinglis/{$repo}.git
 		git push -u origin master
 	end
